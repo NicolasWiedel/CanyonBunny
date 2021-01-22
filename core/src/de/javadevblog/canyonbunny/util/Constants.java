43328b660a1a3c1c0803f4da54127e9f9fdd2082
nicolaswiedel <nicolaswiedel@googlemail.com>
2021-01-22 18:47:36 +0100

    Chapter 4 middle

--- core/src/de/javadevblog/canyonbunny/util/Constants.java
+++ core/src/de/javadevblog/canyonbunny/util/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
 
     // die sichtbare GameWorld ist 5 Meter breit
     public static final float VIEWPORT_HEIGHT = 5.0f;
+
+    // ORt und Name der Atlas - Datei für die Assets
+    public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.atlas";
 }
--- desktop/src/de/javadevblog/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/de/javadevblog/canyonbunny/desktop/DesktopLauncher.java
@@ -2,10 +2,27 @@ package de.javadevblog.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import de.javadevblog.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+
+	private static boolean rebuildAtlas = false;
+	private static boolean drawDebugOutline = true;
+
 	public static void main (String[] arg) {
+
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images",
+					"images", "canyonbunny");
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 480;
