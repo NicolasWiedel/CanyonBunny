5ab072be96e78c200a94aa1ff6273e112e99507d
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2021-01-24 23:10:38 +0100

    Chapter 6 bis creating coin

--- core/src/de/javadevblog/canyonbunny/game/objects/AbstractGameObject.java
+++ core/src/de/javadevblog/canyonbunny/game/objects/AbstractGameObject.java
@@ -1,6 +1,8 @@
 package de.javadevblog.canyonbunny.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class AbstractGameObject {
@@ -10,6 +12,11 @@ public abstract class AbstractGameObject {
     public Vector2 origin;
     public Vector2 scale;
     public float rotation;
+    public Vector2 velocity;
+    public Vector2 terminalVelocity;
+    public Vector2 friction;
+    public Vector2 acceleration;
+    public Rectangle bounds;
 
     public AbstractGameObject() {
         position = new Vector2();
@@ -17,10 +24,49 @@ public abstract class AbstractGameObject {
         origin = new Vector2();
         scale = new Vector2(1, 1);
         rotation = 0;
+        velocity = new Vector2();
+        terminalVelocity = new Vector2(1, 1);
+        friction = new Vector2();
+        acceleration = new Vector2();
+        bounds = new Rectangle();
     }
 
     public void update(float deltaTime){
+        updateMotionX(deltaTime);
+        updateMotionY(deltaTime);
+        // zur neuen Position bewegen
+        position.x += velocity.x * deltaTime;
+        position.y += velocity.y * deltaTime;
+    }
 
+    protected void updateMotionX(float deltaTime) {
+        if(velocity.x != 0){
+            // natürliche Bremse
+            if (velocity.x > 0){
+                velocity.x = Math.max(velocity.x - friction.x * deltaTime, 0);
+            } else {
+                velocity.x = Math.min(velocity.x + friction.x * deltaTime, 0);
+            }
+        }
+        // Beschleunigungn berücksichtigen
+        velocity.x += acceleration.x * deltaTime;
+        // stop bei maximaler Geschwindigkeit
+        velocity.x = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
+    }
+
+    protected void updateMotionY(float deltaTime){
+        if (velocity.y != 0){
+            // natürliche Bremse
+            if (velocity.y > 0){
+                velocity.y = Math.max(velocity.y - friction.y * deltaTime, 0);
+            } else {
+                velocity.y = Math.min(velocity.y + friction.y * deltaTime, 0);
+            }
+        }
+        // Beschleunigungn berücksichtigen
+        velocity.y += acceleration.y * deltaTime;
+        // stop bei maximaler Geschwindigkeit
+        velocity.y = MathUtils.clamp(velocity.y, -terminalVelocity.y, terminalVelocity.y);
     }
 
     public abstract void render (SpriteBatch batch);
