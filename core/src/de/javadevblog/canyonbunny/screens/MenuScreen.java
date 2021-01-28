49f7126e0bacecf99da26a6241fe79224db5de29
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2021-01-28 23:57:34 +0100

    Chapter 8 MenuScreen Aufbau

--- core/src/de/javadevblog/canyonbunny/screens/MenuScreen.java
+++ core/src/de/javadevblog/canyonbunny/screens/MenuScreen.java
@@ -3,10 +3,41 @@ package de.javadevblog.canyonbunny.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import de.javadevblog.canyonbunny.util.Constants;
 
 public class MenuScreen extends AbstractGameScreen {
 
-    
+    private Stage stage;
+    private Skin skinCanyonBunny;
+
+    //Menu
+    private Image imgBackground;
+    private Image imgLogo;
+    private Image imginfo;
+    private Image imgCoin;
+    private Image imgBunny;
+    private Button btnMenuPlay;
+    private Button btnMenuOptions;
+
+    //Optionen
+    private Window winOptions;
+    private TextButton btnWinOptSave;
+    private TextButton btnWinOptCancel;
+    private CheckBox chkSound;
+    private Slider sldSound;
+    private CheckBox chkMusik;
+    private Slider sldMusic;
+    private SelectBox<CharacterSkin> selCharSkin;
+    private Image imgCharSkin;
+    private CheckBox chkShowFpsCounter;
+
+    // debug
+    private final float DEBUG_REBUILD_INTERVAL = 5.0f;
+    private boolean debugEnabled = false;
+    private float debugRebuildStage;
 
     public MenuScreen(Game game){
         super(game);
@@ -20,6 +51,30 @@ public class MenuScreen extends AbstractGameScreen {
         if (Gdx.input.isTouched()){
             game.setScreen(new GameScreen(game));
         }
+    }
+
+    private void RebuildStage() {
+        skinCanyonBunny = new Skin(
+                Gdx.files.internal(Constants.SKIN_CANYONBUNNY_UI),
+                new TextureAtlas(Constants.TEXTURE_ATLAS_UI));
+
+        // Aufbau derLayer
+        Table layerBackground = buildBackgroundLayer();
+        Table layerObjects = buildObjectsLayer();
+        Table layerLogos = buildLogosLayer();
+        Table layerControls = buildControlsLayer();
+        Table layerOptionsWindow = buildOptionsWindowLayer();
+
+        // Stage zusammensetzen
+        stage.clear();
+        Stack stack = new Stack();
+        stage.addActor(stack);
+        stack.setSize(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT);
+        stack.add(layerBackground);
+        stack.add(layerObjects);
+        stack.add(layerLogos);
+        stack.add(layerControls);
+        stage.addActor(layerOptionsWindow);
     }
 
     @Override
