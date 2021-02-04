6a7a8a4ea8e32c72d9667ddd6340a80207123baf
nicolaswiedel <nicolaswiedel@googlemail.com>
2021-02-04 18:19:47 +0100

    Chapter 9 Beginn DirectGame

--- core/src/de/javadevblog/canyonbunny/screens/DirectedGame.java
+++ core/src/de/javadevblog/canyonbunny/screens/DirectedGame.java
@@ -1,6 +1,8 @@
 package de.javadevblog.canyonbunny.screens;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import de.javadevblog.canyonbunny.screens.transitions.ScreenTransition;
@@ -22,6 +24,25 @@ public abstract class DirectedGame implements ApplicationListener {
 
     public void setScreen(AbstractGameScreen screen,
                           ScreenTransition screenTransition){
-
+        int w = Gdx.graphics.getWidth();
+        int h = Gdx.graphics.getHeight();
+        if (!init) {
+            currFbo = new FrameBuffer(Format.RGB888, w, h, false);
+            nextFbo = new FrameBuffer(Format.RGB888, w, h, false);
+            batch = new SpriteBatch();
+            init = true;
+        }
+        // start new transition
+        nextScreen = screen;
+        nextScreen.show(); // activate next screen
+        nextScreen.resize(w, h);
+        nextScreen.render(0); // let screen update() once
+        if (currScreen != null) {
+            currScreen.pause();
+        nextScreen.pause();
+        Gdx.input.setInputProcessor(null); // disable input
+        this.screenTransition = screenTransition;
+        t = 0;
+    }
     }
 }
