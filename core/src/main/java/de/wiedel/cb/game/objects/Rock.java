846df1df6d410b9525bcf539f7c1d31124333765
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-04-17 17:13:04 +0200

    5.1. Rock Klasse

--- core/src/main/java/de/wiedel/cb/game/objects/Rock.java
+++ core/src/main/java/de/wiedel/cb/game/objects/Rock.java
@@ -38,7 +38,7 @@ public class Rock extends AbstractGameObject {
         float relX = 0;
         float relY = 0;
 
-        // linke seite zeichnen
+        // linke Seite zeichnen
         reg = regEdge;
         relX -= dimension.x / 4;
 
@@ -51,5 +51,36 @@ public class Rock extends AbstractGameObject {
             reg.getRegionX(), reg.getRegionY(),
             reg.getRegionWidth(), reg.getRegionHeight(),
             false, false);
+
+        // Mitte zeichnen
+        reg = regMiddle;
+        relX -= 0;
+
+        for (int i = 0; i < length; i++){
+            batch.draw(reg.getTexture(),
+                position.x + relX, position.y + relY,
+                origin.x, origin.y,
+                dimension.x, dimension.y,
+                scale.x, scale.y,
+                rotation,
+                reg.getRegionX(), reg.getRegionY(),
+                reg.getRegionWidth(), reg.getRegionHeight(),
+                false, false);
+            relX += dimension.x;
+        }
+
+        // rechte Seite zeichnen
+        reg = regEdge;
+
+        batch.draw(reg.getTexture(),
+            position.x + relX, position.y + relY,
+            origin.x + dimension.x / 8, origin.y,
+            dimension.x / 4, dimension.y,
+            scale.x, scale.y,
+            rotation,
+            reg.getRegionX(), reg.getRegionY(),
+            reg.getRegionWidth(), reg.getRegionHeight(),
+            true, false);
+
     }
 }
